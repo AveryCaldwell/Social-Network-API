052b7328ba2cd3c0cb131b6e9c2c29baa514564d
Avery <averycaldwell7@gmail.com>
2023-03-21 17:06:50 -0700

    removed activity

--- server.js
+++ server.js
@@ -2,18 +2,8 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-// Run npm install mongodb and require mongodb and MongoClient class
-// const { MongoClient } = require('mongodb');
-
-const cwd = process.cwd();
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('Social-Network-API')
-    ? cwd.split('/Social-Network-API/')[1]
-    : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,6 +11,6 @@ app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
-        console.log(`API server for ${activity} running on port ${PORT}!`);
+        console.log(`API server running on port ${PORT}!`);
     });
 });
